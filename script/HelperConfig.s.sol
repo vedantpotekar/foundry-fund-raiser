bc6605d211ef6f1b2d86b247e2500528d5342f50
vedantpotekar <vedantpotekar@gmail.com>
2023-08-04 03:31:51 +0530

    54_33

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    NetworkConfig public activeNetworkConfig;
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if(block.chainid == 11155111){
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    } 
+    function getAnvilConfig() public pure returns (NetworkConfig memory){
+
+    } 
+}
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,8 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-test/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
 
+    function testMinDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+    function testPriceFeedVersion() public {
+        assertEq(fundMe.getVersion(),4);
+    }
 }
